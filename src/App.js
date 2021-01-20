4f8bace34b060accaef088583123ff96216f4f45
Beardless-sheik <nyirenda_a@zoho.com>
2021-01-20 13:05:53 +0200

    Added "Banner" component for Homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./style.css";
 
+//Importing Components
+import Banner from "./Components/Banner"
+
 export default function App() {
   return (
     <div>
+      <Banner />
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
---
+++ src/Components/banner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Pane, Text } from "./Componets/Banner";
+
+function Banner() {
+  return(
+    <Pane>
+      <Text> Banner Text </Text>
+    </Pane>
+  )
+}
